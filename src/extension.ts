e46959d9d4c8b38057bc29a88479280bc59e4fe2
Kai Wood <kwood@kwd.io>
2016-10-22 15:44:25 +0200

    Big refactor step

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,34 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// import * as vscode from 'vscode';
+import {
+    window,
+    commands,
+    Disposable,
+    ExtensionContext,
+    StatusBarAlignment,
+    StatusBarItem,
+    TextDocument,
+    Position,
+    Range,
+    Selection,
+} from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    let indentationLevelMover = new IndentationLevelMover();
 
-    let moveDown = vscode.commands.registerCommand('extension.moveDown', () => {
-        moveDownUntilCharClassChanges();   
+    var moveDown = commands.registerCommand('extension.moveDown', () => {
+        indentationLevelMover.moveDown();
     });
 
-    let moveUp = vscode.commands.registerCommand('extension.moveUp', () => {
-        moveUpUntilCharClassChanges();
-    });
+    var moveUp = commands.registerCommand('extension.moveUp', () => {
+        indentationLevelMover.moveUp();
+    })
 
+    context.subscriptions.push(indentationLevelMover);
     context.subscriptions.push(moveDown);
     context.subscriptions.push(moveUp);
 }
@@ -23,121 +37,103 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function charBeforeCursor() {
-    let selection = vscode.window.activeTextEditor.selection;
 
-    let endPosition = new vscode.Position(selection.end.line, selection.end.character + 1);
-    let range = new vscode.Range(selection.start, endPosition);
+class IndentationLevelMover {
+    public moveDown() {
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-    let char = vscode.window.activeTextEditor.document.getText(range)
-    return char;
-}
+        let currentLineNumber = editor.selection.start.line;
+        let currentLevel = this.indentationLevelForLine(currentLineNumber);
 
-function nextCharBeforeCursor() {
-    let selection = vscode.window.activeTextEditor.selection;
-
-    let startPosition = new vscode.Position(selection.start.line + 1, selection.start.character);
-    let endPosition = new vscode.Position(selection.end.line + 1, selection.end.character + 1);
-    let range = new vscode.Range(startPosition, endPosition);
-
-    let char = vscode.window.activeTextEditor.document.getText(range)
-    return char;
-}
-
-function previousCharBeforeCursor() {
-    let selection = vscode.window.activeTextEditor.selection;
-
-    let startPosition = new vscode.Position(selection.start.line - 1, selection.start.character);
-    let endPosition = new vscode.Position(selection.end.line - 1, selection.end.character + 1);
-    let range = new vscode.Range(startPosition, endPosition);
-
-    let char = vscode.window.activeTextEditor.document.getText(range)
-    return char;
-}
-
-function isWhitespace(char) {
-    return char.search(/\w/) >= 0 ? false : true
-}
-
-function isLastLine() {
-    let lineCount = vscode.window.activeTextEditor.document.lineCount
-    let currentLineNumber = vscode.window.activeTextEditor.selection.end.line + 1
-    return lineCount === currentLineNumber ? true : false;
-}
-
-function isFirstLine() {
-    return vscode.window.activeTextEditor.selection.end.line === 0 ? true : false
-}
-
-let movesDownAtBlock = false;
-function moveDownUntilCharClassChanges() {
-    if (isLastLine()) {
-        return;
+        let position = editor.selection.active;
+        let newPosition = position.with(this.findNextLine(currentLineNumber, currentLevel), currentLevel);
+        let selection = new Selection(newPosition, newPosition);
+        editor.selection = selection;
     }
 
-    if (isWhitespace(charBeforeCursor()) && isWhitespace(nextCharBeforeCursor())) {
+    public moveUp() {
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-        vscode.commands.executeCommand("cursorDown").then(function () {
-            movesDownAtBlock = true;
-            moveDownUntilCharClassChanges();
-        });
+        let currentLineNumber = editor.selection.start.line;
+        let currentLevel = this.indentationLevelForLine(currentLineNumber);
 
-    } else if (!isWhitespace(charBeforeCursor()) && !isWhitespace(nextCharBeforeCursor())) {
-
-        vscode.commands.executeCommand("cursorDown").then(function () {
-            movesDownAtBlock = true;
-            moveDownUntilCharClassChanges();
-        });
-
-    } else if (!isWhitespace(charBeforeCursor()) && isWhitespace(nextCharBeforeCursor()) && !movesDownAtBlock) {
-
-        vscode.commands.executeCommand("cursorDown").then(function () {
-            movesDownAtBlock = true;
-            moveDownUntilCharClassChanges();
-        });
-
-    } else if (isWhitespace(charBeforeCursor()) && !isWhitespace(nextCharBeforeCursor())) {
-        movesDownAtBlock = false;
-        vscode.commands.executeCommand("cursorDown")
-
+        let position = editor.selection.active;
+        let newPosition = position.with(this.findPreviousLine(currentLineNumber, currentLevel), currentLevel);
+        let selection = new Selection(newPosition, newPosition);
+        editor.selection = selection;
     }
 
-    movesDownAtBlock = false;
-}
+    public indentationLevelForLine(lineToCheck) {
+        let editor = window.activeTextEditor;
+        let line = editor.document.lineAt(lineToCheck);
 
-let movesUpAtBlock = false;
-function moveUpUntilCharClassChanges() {
-    if (isFirstLine()) {
-        return;
+        if (line.text.toString().length === 0) { // TODO check for whitespace-only lines as well
+            return -1;
+        } else {
+            return line.firstNonWhitespaceCharacterIndex;
+        }
     }
 
-    if (isWhitespace(charBeforeCursor()) && isWhitespace(previousCharBeforeCursor())) {
+    public findNextLine(currentLineNumber, currentIndentationLevel: Number) {
+        let editor = window.activeTextEditor;
+        let indentationChanged = false;
+        let changedAtFirstCheckedLine = false;
 
-        vscode.commands.executeCommand("cursorUp").then(function () {
-            movesUpAtBlock = true;
-            moveUpUntilCharClassChanges();
-        });
+        for (let lineNumber = currentLineNumber + 1; lineNumber < editor.document.lineCount; lineNumber++) {
+            let indentationForLine = this.indentationLevelForLine(lineNumber);
 
-    } else if (!isWhitespace(charBeforeCursor()) && !isWhitespace(previousCharBeforeCursor())) {
+            if (currentIndentationLevel !== indentationForLine) {
+                indentationChanged = true;
+            }
 
-        vscode.commands.executeCommand("cursorUp").then(function () {
-            movesUpAtBlock = true;
-            moveUpUntilCharClassChanges();
-        });
+            if (indentationChanged && lineNumber === currentLineNumber + 1) {
+                changedAtFirstCheckedLine = true;
+            }
 
-    } else if (!isWhitespace(charBeforeCursor()) && isWhitespace(previousCharBeforeCursor()) && !movesUpAtBlock) {
+            // Don't count blank/empty lines
+            if (indentationForLine < 0) {
+                continue;
+            }
 
-        vscode.commands.executeCommand("cursorUp").then(function () {
-            movesUpAtBlock = true;
-            moveUpUntilCharClassChanges();
-        });
-
-    } else if (isWhitespace(charBeforeCursor()) && !isWhitespace(previousCharBeforeCursor())) {
-
-        movesUpAtBlock = false;
-        vscode.commands.executeCommand("cursorUp")
-
+            if (indentationChanged && indentationForLine <= currentIndentationLevel) {
+                return changedAtFirstCheckedLine ? lineNumber : lineNumber - 1;
+            }
+        }
     }
 
-    movesUpAtBlock = false;
+    public findPreviousLine(currentLineNumber, currentIndentationLevel: Number) {
+        let editor = window.activeTextEditor;
+        let indentationChanged = false;
+        let changedAtFirstCheckedLine = false;
+
+        for (let lineNumber = currentLineNumber + 1; lineNumber > 0; lineNumber--) {
+            let indentationForLine = this.indentationLevelForLine(lineNumber);
+
+            if (currentIndentationLevel !== indentationForLine) {
+                indentationChanged = true;
+            }
+
+            if (indentationChanged && lineNumber === currentLineNumber - 1) {
+                changedAtFirstCheckedLine = true;
+            }
+
+            // Don't count blank/empty lines
+            if (indentationForLine < 0) {
+                continue;
+            }
+
+            if (indentationChanged && indentationForLine <= currentIndentationLevel) {
+                return changedAtFirstCheckedLine ? lineNumber : lineNumber - 1;
+            }
+        }
+    }
+
+    dispose() {
+    }
 }
